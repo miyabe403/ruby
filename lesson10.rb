3d7cd132603f64b6462baf472357e8d48bc4dccc
miyabe403 <108040533+miyabe403@users.noreply.github.com>
2023-07-30 14:15:56 +0000

    [Finish] ruby-10

--- lesson10.rb
+++ lesson10.rb
@@ -7,5 +7,5 @@ end
 class Truck < Car  # class 子クラス < 親クラス
 end
 
-truck = Truck.new  
-truck.run(5)
+truck = Truck.new  # Truck.newでインスタンスの作成 変数にtruckと設定
+truck.run(5)  # truck.runでメソッドを呼び出して、引数に5を代入
